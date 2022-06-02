749d05c474c989f237939e8b65d03a14848a9a77
txt5 <105361005+GunelAhmadova@users.noreply.github.com>
2022-06-02 06:03:41 +0400

    Add files via upload

---
+++ 2 iyun/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _2_iyun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User(Console.ReadLine());
+            user1.Password = Console.ReadLine();
+            Console.WriteLine(user1.UserName+ " "+ user1.Password);
+            
+        }
+    }
+}
---
+++ 2 iyun/User.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2_iyun
+{
+    class User
+    {
+        private string _userName;
+        private string _password; 
+
+        public User(string UserName)
+        {
+            _userName = UserName;
+          
+        } 
+        public string UserName
+        {
+            get
+            {
+                return _userName;
+            } set
+            {
+                if (6<value.Length&&value.Length<25)
+                {
+                    _userName = value;
+                }
+            }
+        } 
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if (8<value.Length&&25>value.Length)
+                { if (HasDigit(value)&& HasUpper(value)&&HasLower(value))
+                    {
+                        _password = value;
+                    }
+                    
+                }
+            }
+        } 
+        public bool HasDigit (string str)
+        {
+            
+            bool check = false;
+            for (int i = 0; i < str.Length; i++)
+
+            { if (Char.IsDigit(str[i]))
+                {
+                    check = true;
+                    break;
+                } 
+
+            } return check;
+
+        } 
+        public bool HasUpper (string upper)
+        {
+            bool check = false;
+            for (int i = 0; i < upper.Length; i++)
+            { if (Char.IsUpper(upper[i]))
+                {
+                    check = true;
+                    break;
+                }
+
+            } return check;
+
+        } 
+        public bool HasLower (string lower)
+        {
+            bool check = false;
+            for (int i = 0; i < lower.Length; i++)
+            { if (Char.IsLower(lower[i]))
+                {
+                    check = true;
+                    break;
+                }
+
+            } return check;
+        }
+
+
+    }
+}
